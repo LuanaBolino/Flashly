11cb0dcfb9ad3f00270588371199335a5f87819a
luana <luanabolino.dev@gmail.com>
2024-12-13 18:58:57 -0300

    atividade-08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import { LoremComponent } from './lorem/lorem.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloComponent, LoremComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/lorem/lorem.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lorem',
+  imports: [],
+  templateUrl: './lorem.component.html',
+  styleUrl: './lorem.component.css'
+})
+export class LoremComponent {
+
+}
